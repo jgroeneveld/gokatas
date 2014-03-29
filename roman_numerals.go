b80fdc1a5153654e0f94312fc3254058439f395c
Jaap Groeneveld <jgroeneveld@me.com>
2014-03-29 12:26:38 +0100

    added tests for IsValidRomanNumeral

--- roman_numerals.go
+++ roman_numerals.go
@@ -3,3 +3,7 @@ package gokatas
 func FromRomanNumerals(roman string) int {
 	return 0
 }
+
+func IsvalidRomanNumeral(roman string) bool {
+	return true
+}
---
+++ roman_numerals_test.go
@@ -0,0 +1,48 @@
+package gokatas
+
+import (
+	gc "launchpad.net/gocheck"
+)
+
+// http://de.scribd.com/doc/140927641/Function-Kata-From-Roman-Numerals
+type RomanNumeralsSuite struct{}
+
+func (s *RomanNumeralsSuite) Test_FromRomanNumerals(c *gc.C) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{"I", 1},
+		{"II", 2},
+		{"IV", 4},
+		{"V", 5},
+		{"IX", 9},
+		{"XLII", 42},
+		{"XCIX", 99},
+		{"MMXIII", 2013},
+	}
+
+	for _, test := range tests {
+		c.Check(FromRomanNumerals(test.in), gc.Equals, test.out,
+			gc.Commentf("for input '%s'", test.in))
+	}
+}
+
+func (s *RomanNumeralsSuite) Test_IsValidRomanNumeral(c *gc.C) {
+	tests := []struct {
+		in  string
+		out bool
+	}{
+		{"I", true},
+		{"XCIX", true},
+		{"I I", false}, // syntactic
+		{"IC", false},  // semantic error
+	}
+
+	for _, test := range tests {
+		c.Check(IsvalidRomanNumeral(test.in), gc.Equals, test.out,
+			gc.Commentf("for input '%s'", test.in))
+	}
+}
+
+var _ = gc.Suite(&RomanNumeralsSuite{})
