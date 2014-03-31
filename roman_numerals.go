45cebeaf87f6a2cee559bc9fd91f034cb8beabbe
Jaap Groeneveld <jgroeneveld@me.com>
2014-03-31 20:37:04 +0200

    added FromRomanNumerals implementation

--- roman_numerals.go
+++ roman_numerals.go
@@ -1,9 +1,48 @@
 package gokatas
 
+import (
+	"strings"
+)
+
 func FromRomanNumerals(roman string) int {
-	return 0
+	roman, subSum := romanSumForMap(roman, romanSubtractionValues())
+	roman, addSum := romanSumForMap(roman, romanAdditionValues())
+
+	return subSum + addSum
 }
 
 func IsvalidRomanNumeral(roman string) bool {
 	return true
+}
+
+func romanSubtractionValues() map[string]int {
+	return map[string]int{
+		"IV": 4,
+		"IX": 9,
+		"XL": 40,
+		"XC": 90,
+		"CD": 400,
+		"CM": 900,
+	}
+}
+
+func romanAdditionValues() map[string]int {
+	return map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+}
+
+func romanSumForMap(roman string, dict map[string]int) (string, int) {
+	sum := 0
+	for k, v := range dict {
+		sum += strings.Count(roman, k) * v
+		roman = strings.Replace(roman, k, "", -1)
+	}
+	return roman, sum
 }
--- roman_numerals_test.go
+++ roman_numerals_test.go
@@ -7,21 +7,32 @@ import (
 // http://de.scribd.com/doc/140927641/Function-Kata-From-Roman-Numerals
 type RomanNumeralsSuite struct{}
 
-func (s *RomanNumeralsSuite) Test_FromRomanNumerals(c *gc.C) {
-	c.Skip("Not yet implemented")
-
+func (s *RomanNumeralsSuite) Test_FromRomanNumeralsAddition(c *gc.C) {
 	tests := []struct {
 		in  string
 		out int
 	}{
 		{"I", 1},
 		{"II", 2},
-		{"IV", 4},
 		{"V", 5},
+		{"MMXIII", 2013},
+	}
+
+	for _, test := range tests {
+		c.Check(FromRomanNumerals(test.in), gc.Equals, test.out,
+			gc.Commentf("for input '%s'", test.in))
+	}
+}
+
+func (s *RomanNumeralsSuite) Test_FromRomanNumeralsSubtraction(c *gc.C) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{"IV", 4},
 		{"IX", 9},
 		{"XLII", 42},
 		{"XCIX", 99},
-		{"MMXIII", 2013},
 	}
 
 	for _, test := range tests {
